602226bdcaa97c4e75ccb18494f09b35aa6fe9db
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-12-12 09:29:05 +0100

    Cambio a Byte y implementado un foreach

--- src/ejercicio1/Main.java
+++ src/ejercicio1/Main.java
@@ -3,16 +3,24 @@ package ejercicio1;
 public class Main {
     public static void main(String[] args) {
         // ^ Definimos un array entero de 10 posiciones
-        int[] numerosAleatorios = new int[10];
+        byte[] numerosAleatorios = new byte[10];
         
+        // ^ Definimos una variable posicion para mostrar la posicion del array
+        byte posicion = 0;
+
         // ! Mediante un bucle for generaremos 10 numeros aleatorios y se los asignaremos al array
-        for (int i = 0; i < numerosAleatorios.length; i++) {
-            numerosAleatorios[i] = (int) (Math.random() * (100 - 1));
+        for (byte i = 0; i < numerosAleatorios.length; i++) {
+            numerosAleatorios[i] = (byte) (Math.random() * (100 - 1) + 1);
         }
 
-        // ! Mediante un bucle for mostraremos los valores del array
-        for (int i = 0; i < numerosAleatorios.length; i++) {
-            System.out.println("[" + i + "] > " + numerosAleatorios[i]);
+        // ! Mediante un bucle for each mostraremos los valores del array
+        System.out.println("Los numeros aleatorios generados son: ");
+        for (byte numero : numerosAleatorios) {
+            // ! Imprimimos el valor del array
+            System.out.println("[" + posicion + "] > " + numero);
+
+            // ! Incrementamos la posicion
+            posicion++;
         }
     }
 }
